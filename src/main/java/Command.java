020622da083b4d6c93f43a7db3251f4601171356
wuweishuo <378960446@qq.com>
2023-12-13 16:53:31 +0800

    import lombok

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,28 +1,18 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@AllArgsConstructor
 public class Command {
 
+    @Getter
+    @Setter
     private Operator operator;
+
+    @Getter
+    @Setter
     private BigDecimal operand;
 
-    public Command(Operator operator, BigDecimal operand) {
-        this.operator = operator;
-        this.operand = operand;
-    }
-
-    public Operator getOperator() {
-        return operator;
-    }
-
-    public void setOperator(Operator operator) {
-        this.operator = operator;
-    }
-
-    public BigDecimal getOperand() {
-        return operand;
-    }
-
-    public void setOperand(BigDecimal operand) {
-        this.operand = operand;
-    }
 }
